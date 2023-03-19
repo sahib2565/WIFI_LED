876fd17421caef7f220d99ac6b2be393ae22491c
Sahib Singh <85299873+sahib2565@users.noreply.github.com>
2023-03-19 21:04:52 +0100

    Add files via upload

---
+++ WIFI_LED/README.md
@@ -0,0 +1,20 @@
+# WIFI LED
+
+![language](https://img.shields.io/badge/language-c%2B%2B-red)
+![Testing](https://img.shields.io/badge/Test-Pass-green)
+## Description
+In this project we are going to switch on and off a LED via a website,using the WiFi module that is present inside the ESP32.
+
+## Getting started
+
+You just have to connect the led to the ESP32 as shown in the schematics,once you do that install the libraries needed for the project and install the ESP32 devkit board
+
+### Modify this part of the code
+```c++
+const char* ssid = ""; // the name of your WiFi
+const char* password = ""; // the password of your WiFi
+```
+
+## Author
+
+### Il_kima
---
+++ WIFI_LED/esp32/main.cpp
@@ -0,0 +1,76 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <ArduinoJson.h>
+
+// Replace with your network credentials
+const char* ssid = "Wind3 HUB - EE2DCC";
+const char* password = "2bc45et75b9es2cf";
+
+// Create an instance of the WebServer
+WebServer server(80);
+
+// Define the endpoint for receiving switch state data
+const char* switchEndpoint = "/switch";
+
+// Define a variable to store the current switch state
+bool switchState = false;
+
+void handleSwitch() {
+  // Parse the incoming JSON data
+  DynamicJsonDocument json(1024);
+  deserializeJson(json, server.arg("plain"));
+
+  // Extract the switch state from the JSON data
+  bool newState = json["plain"];
+
+  //Serial.println(newState);
+
+  // Perform the desired action based on the switch state
+  if (newState != switchState) {
+    switchState = newState;
+
+    if (switchState) {
+      // Do something when the switch is turned on
+      digitalWrite(23,HIGH);
+      Serial.println("ON");
+    } else {
+      // Do something when the switch is turned off
+      digitalWrite(23,LOW);
+      Serial.println("OFF");
+    }
+  }
+
+  // Send a response back to the client
+  server.send(200, "text/plain", "OK");
+}
+
+
+void setup() {
+  // Start the serial communication
+  Serial.begin(9600);
+  pinMode(23, OUTPUT);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  // Print the IP address
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Register the switch endpoint handler
+  server.on(switchEndpoint, handleSwitch);
+
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+}
+
+void loop() {
+  // Handle incoming client requests
+  server.handleClient();
+}
